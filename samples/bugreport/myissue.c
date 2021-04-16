e374ce0f6e0593a6ae031f27ab310c75d6f01480
Ben Vanik <ben.vanik@gmail.com>
2021-04-16 10:31:32 -0700

    Bug report for issue #179 (vector alignment issues).

--- samples/bugreport/myissue.c
+++ samples/bugreport/myissue.c
@@ -2,6 +2,8 @@
 #include "build/myissue_generated.h"
 #include "flatcc/support/hexdump.h"
 
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
     int ret;
@@ -15,16 +17,45 @@ int main(int argc, char *argv[])
     B = &builder;
     flatcc_builder_init(B);
 
+    #define BUFFER_COUNT 5
+    const int sizes[BUFFER_COUNT] = {
+      1,
+      16,
+      22,
+      55,
+      123,
+    };
+    Eclectic_Buffer_ref_t buffer_refs[BUFFER_COUNT];
+    for (int i = 0; i < BUFFER_COUNT; ++i) {
+      fprintf(stdout, "pre start min_align: %d\n", B->min_align);
+      flatcc_builder_start_vector(B, 1, /*align=*/16, FLATBUFFERS_COUNT_MAX(1));
+      fprintf(stdout, "post start min_align: %d\n", B->min_align);
+      uint8_t* p = flatcc_builder_extend_vector(B, sizes[i]);
+      memset(p, 'x', sizes[i]);
+      fprintf(stdout, "pre end min_align: %d\n", B->min_align);
+      flatcc_builder_ref_t data = flatcc_builder_end_vector(B);
+      fprintf(stdout, "post end min_align: %d\n", B->min_align);
+      Eclectic_Buffer_start(B);
+      Eclectic_Buffer_data_add(B, data);
+      buffer_refs[i] = Eclectic_Buffer_end(B);
+    }
+    flatcc_builder_ref_t buffers_vec = flatcc_builder_create_offset_vector(B, buffer_refs, BUFFER_COUNT);
+
     Eclectic_FooBar_start_as_root(B);
     Eclectic_FooBar_say_create_str(B, "hello");
     Eclectic_FooBar_meal_add(B, Eclectic_Fruit_Orange);
     Eclectic_FooBar_height_add(B, -8000);
+    Eclectic_FooBar_buffers_add(B, buffers_vec);
+
+    fprintf(stdout, "pre root end min_align: %d\n", B->min_align);
     Eclectic_FooBar_end_as_root(B);
+    fprintf(stdout, "post root end min_align: %d\n", B->min_align);
     buf = flatcc_builder_get_direct_buffer(B, &size);
-#if defined(PROVOKE_ERROR) || 0
-    /* Provoke error for testing. */
-    ((char*)buf)[0] = 42;
-#endif
+
+    FILE* f = fopen("output.bin", "wb");
+    fwrite(buf, 1, size, f);
+    fclose(f);
+
     ret = Eclectic_FooBar_verify_as_root(buf, size);
     if (ret) {
         hexdump("Eclectic.FooBar buffer for myissue", buf, size, stdout);
